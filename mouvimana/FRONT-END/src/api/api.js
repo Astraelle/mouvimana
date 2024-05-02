0bbe2244813e02cc64cc6a4bc835e3f2dbe265e3
Charlie <charlie.pierre@orange.fr>
2024-05-02 09:54:54 +0200

    Ajout des dépendances tailwind

---
+++ mouvimana/FRONT-END/src/api/api.js
@@ -0,0 +1,6 @@
+export const URL = {
+    FILM_GET: "http://localhost:8000/api/film/get",
+    USER_GET: "http://localhost:8000/api/user/get",
+    USER_SIGNUP: "http://localhost:8000/api/user/signup",
+    USER_SIGNIN: "http://localhost:8000/api/user/signin"
+}
