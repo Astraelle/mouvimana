fdea8f240d729574694e3a4c9953284cfce3aa9d
Astraelle <charlie-pierre@outlook.fr>
2024-05-03 17:58:29 +0200

    (temporaire) mise en place des routes

--- mouvimana/FRONT-END/src/App.js
+++ mouvimana/FRONT-END/src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom'
+import Header from './components/header';
+import Home from './pages/home';
+import Films from './pages/films';
+import About from './pages/about';
+import Footer from './components/footer';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='text-3xl font-bold underline'>
-        Hello World !
-      </h1>
+    <div className="before:box-border after:box-border before:m-0 after:p-0">
+      <Routes>
+        <Route path='/' element={<Header/>}>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/films' element={<Films/>}/>
+          <Route path='/about' element={<About/>}/>
+        </Route>
+      </Routes>
+      <Footer/>
     </div>
   );
 }
