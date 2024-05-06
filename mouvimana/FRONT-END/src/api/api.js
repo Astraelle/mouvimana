6f5e2f7c3ff6cbddd96c39fbeaee2a7667ec68e1
Charlie <charlie.pierre@orange.fr>
2024-05-06 14:49:43 +0200

    Renommage d'url de USER_GET et ajout de nouvelles url

--- mouvimana/FRONT-END/src/api/api.js
+++ mouvimana/FRONT-END/src/api/api.js
@@ -1,6 +1,8 @@
 export const URL = {
     FILM_GET: "http://localhost:8000/api/film/get",
-    USER_GET: "http://localhost:8000/api/user/get",
+    FILM_SEARCH: "http://localhost:8000/api/film/search",
+    FILM_ID: "http://localhost:8000/api/film/details",
+    USER_GET: "http://localhost:8000/api/user/getuser",
     USER_SIGNUP: "http://localhost:8000/api/user/signup",
     USER_SIGNIN: "http://localhost:8000/api/user/signin"
 }
