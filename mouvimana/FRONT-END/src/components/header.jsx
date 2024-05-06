bce35e88bd565be88cab1fbbaf178245b69ad4e8
Charlie <charlie.pierre@orange.fr>
2024-05-06 14:55:56 +0200

    Ajout de conditions d'affichage + composant reherche

--- mouvimana/FRONT-END/src/components/header.jsx
+++ mouvimana/FRONT-END/src/components/header.jsx
@@ -1,20 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Outlet, Link } from 'react-router-dom'
+import MovieSearch from './search';
 
 const Header = () => {
+
+    useEffect(() =>{
+        isLoggedIn();
+      }, [])
+    
+      const [LoggedIn, setLoggedIn] = useState(false);
+    
+      const isLoggedIn = async () =>{
+        try {
+          const storage = localStorage.getItem('token') || localStorage.getItem('adminToken');
+          if (storage) {
+            setLoggedIn(true)
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      }
   return (
     <div>
-        <nav className='grid grid-cols-[150px_minmax(0,1fr)] grid-rows-1 bg-[#740067] fixed w-screen h-[6vh]'>
+        <nav className='grid grid-cols-[200px_minmax(0,1fr)_500px] grid-rows-1 bg-[#460074] border-b-[#740067] border-b-4 fixed w-screen h-[55px]'>
             <ul className='grid grid-cols-1 grid-row-1 justify-items-center items-center'>
                 <li><Link to="/" className='text-[#fafafa] text-center'>Mouvimana</Link></li>
             </ul>
-            <ul className='grid grid-cols-3 grid-row-1 justify-items-center items-center'>
+            <div className='grid justify-items-end'>
+              <MovieSearch/>
+            </div>
+            <ul className='grid grid-cols-3 grid-row-1 justify-items-center content-center'>
                 <li><Link to="/films" className='text-[#fafafa] text-center'>Films</Link></li>
                 <li><Link to="/about" className='text-[#fafafa] text-center'>A propos</Link></li>
-                <li><Link to="/profile" className='text-[#fafafa] text-center'>Profil</Link></li>
+                {LoggedIn ?
+                    <li><Link to="/profil" className='text-[#fafafa] text-center'>Profil</Link></li>
+                : 
+                    <li><Link to="/signin" className='text-[#fafafa] text-center'>Se connecter</Link></li>
+                }
+                
             </ul>
         </nav>
-        <section className=''>
+        <section>
             <Outlet/>
         </section>
     </div>
