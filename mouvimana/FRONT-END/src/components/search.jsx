ad606e51a63e91bf4405e5b6982c2ea63d1f1664
Charlie <charlie.pierre@orange.fr>
2024-05-06 14:56:39 +0200

    correction d'un bug où le résultat ne s'affichait pas correctement

--- mouvimana/FRONT-END/src/components/search.jsx
+++ mouvimana/FRONT-END/src/components/search.jsx
@@ -1,16 +1,21 @@
 import React, { useState } from 'react'
 import axios from 'axios';
+import { URL } from '../api/api';
+import { Link } from 'react-router-dom'
 
 const MovieSearch = () => {
 
     const [searchMovie, setSearchMovie] = useState("");
     const [resultMovie, setResultMovie] = useState([]);
-    const [typeTimeout, setTypeTimeout] = useState(null);
 
-    const handleSearch = async () => {
+    const handleSearch = async (term) => {
         try {
-            const response = await axios.get(`api/film/get?q=${searchMovie}`);
-            setResultMovie(response.data);
+            if (term.trim() === '') {
+                setResultMovie([]);
+                return;
+            }
+            const response = await axios.get(`${URL.FILM_SEARCH}?q=${term}`);
+            setResultMovie(response.data.slice(0, 3));
         } catch (error) {
             console.error(error);
         }
@@ -19,28 +24,27 @@ const MovieSearch = () => {
     const handleInputChange = (e) =>{
         const input = e.target.value;
         setSearchMovie(input);
-        clearTimeout(typeTimeout);
-        const timeout = setTimeout(() =>{
-            handleSearch(input)
-        }, 300)
-        setTypeTimeout(timeout);
+        handleSearch(input);
     }
 
   return (
-    <>
+    <div className='grid grid-rows-[50px_minmax(0,1fr)] grid-cols-1 items-center w-[200px]'>
         <input 
             type="text" 
             aria-label='search button'
             value={searchMovie}
             onChange={handleInputChange}
-            placeholder='Rechercher'
+            placeholder='RECHERCHER'
+            className='focus:border-[#fafafa] rounded-[5px] bg-[#460074] placeholder:text-[#fafafa] text-[#fafafa] h-[100%]'
         />
-        <ul>
+        <ul className='text-clip bg-[#460074] grid rounded-[10px] '>
             {resultMovie.map(movie => (
-                <li key={movie.id}>{movie.titre}</li>
+                <li key={movie.id} className='text-clip pl-[5px] h-[50px] grid items-center border-t-[#740067] border-t-2'>
+                    <Link to={`/film/${movie._id}`} className='text-[#fafafa]'>{movie.titre}</Link>
+                </li>
             ))}
         </ul>
-    </>
+    </div>
   )
 }
 
