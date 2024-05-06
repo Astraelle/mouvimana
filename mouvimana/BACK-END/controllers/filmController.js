40110a126b3f1849500c13c1dcca48610ec7dcfa
Charlie <charlie.pierre@orange.fr>
2024-05-06 14:54:43 +0200

    Ajout du filtre des doublons

--- mouvimana/BACK-END/controllers/filmController.js
+++ mouvimana/BACK-END/controllers/filmController.js
@@ -17,8 +17,26 @@ const excelData = () =>{
     }
 }
 
-const excelFilter = () =>{
+// Enlève tous les doublons
+const excelDuplicate = () =>{
     const data = excelData();
+    const uniqueData = [];
+    const uniqueValue = new Set();
+    data.forEach(row =>{
+        if (!uniqueValue.has(row.Titre)) {
+            uniqueValue.add(row.Titre);
+            uniqueData.push(row);
+        }
+    })
+    const duplicates = data.length - uniqueData.length;
+    console.log(uniqueData);
+    console.log(duplicates);
+    return uniqueData;
+}
+
+// Filtre les données pour le bon schéma
+const excelFilter = () =>{
+    const data = excelDuplicate();
     const formatData = data.map(movie =>{
         const regex = /<[^>]*>/g;
         return{
@@ -62,5 +80,25 @@ export const getFilm = async (req, res) =>{
         res.status(200).json(getFilmData);
     } catch (err) {
         res.status(500).json("Erreur lors de la récupération des données", err)
+    }
+}
+
+export const searchFilmByTitle = async (req, res) =>{
+    const searchName = req.query.q;
+    try {
+        const movies = await Film.find({ titre: {$regex: searchName, $options: 'i'}});
+        res.json(movies);
+    } catch (error) {
+        res.status(500).json("Erreur lors de la recherche du film", error);
+    }
+}
+
+export const searchFilmById = async (req, res) =>{
+    const searchId = req.params.id;
+    try {
+        const movies = await Film.findById(searchId);
+        res.json(movies);
+    } catch (error) {
+        res.status(500).json("Erreur lors l'extraction des details du film", error);
     }
 }
