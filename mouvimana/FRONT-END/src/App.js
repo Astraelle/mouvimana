42c3d2f2bb6c91b935af6b993bc37336f6e28a55
Charlie <charlie.pierre@orange.fr>
2024-05-06 14:47:33 +0200

    Ajout de nouvelles routes

--- mouvimana/FRONT-END/src/App.js
+++ mouvimana/FRONT-END/src/App.js
@@ -5,6 +5,10 @@ import Home from './pages/home';
 import Films from './pages/films';
 import About from './pages/about';
 import Footer from './components/footer';
+import FilmDetails from './pages/filmDetails';
+import SignIn from './pages/signIn';
+import SignUp from './pages/signUp';
+import Profile from './pages/profile';
 
 function App() {
   return (
@@ -14,6 +18,10 @@ function App() {
           <Route path='/' element={<Home/>}/>
           <Route path='/films' element={<Films/>}/>
           <Route path='/about' element={<About/>}/>
+          <Route path='/profil' element={<Profile/>}/>
+          <Route path='/signin' element={<SignIn/>}/>
+          <Route path='/signup' element={<SignUp/>}/>
+          <Route path='/film/:id' element={<FilmDetails/>}/>
         </Route>
       </Routes>
       <Footer/>
